cad8dcc0b2fddc2da57d0ef5860fe61040579a48
zahidnazir.khan <zahidnazir.khan@sysemsltd.com>
2022-12-09 17:55:57 +0500

    Create Example to upload

---
+++ app/src/main/java/com/example/dagger_di/EmailService.kt
@@ -0,0 +1,7 @@
+package com.example.dagger_di
+
+class EmailService {
+    fun send(message : String){
+
+    }
+}
---
+++ app/src/main/java/com/example/dagger_di/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.example.dagger_di
+
+import javax.inject.Inject
+
+class UserRepository {
+    fun saveUser(name : String, email : String){
+
+    }
+}
